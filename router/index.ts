99d8d6e06d533915666c6b3281baff7293e2071d
Naseebullah-Wali <70603493+Naseebullah-Wali@users.noreply.github.com>
2024-03-08 01:09:27 +0300

    routers index.ts updated

--- router/index.ts
+++ router/index.ts
@@ -2,12 +2,31 @@ import express from "express"
 import userRoute from "./userEndpoints"
 import movieRouter from "./movies"
 import verifyToken from "../middleware/tokenVerification"
-
+import categoryRouter from "./categoriesEndPoints";
+import warehouseRouter from "./warehousesEndPoints";
+import supplierRouter from "./supplierEndPoints";
+import orderRouter from "./orderEndPoints";
+import orderDetailRouter from "./orderDetailsEndPoints";
+import customerRouter from "./customerEndPoints";
+import employeeRouter from "./employeeEndpoints";
+import saleRouter from "./salesEndPoints";
+import saleDetailRouter from "./salesDetailsEndpoints";
+import productWarehouseRouter from "./productwarehousesEndPoints";
 
 const router: express.Router = express.Router()
 
 router.use('/', userRoute)
 router.use('/',movieRouter)
+router.use('/', categoryRouter);
+router.use('/', warehouseRouter);
+router.use('/', supplierRouter);
+router.use('/', orderRouter);
+router.use('/', orderDetailRouter);
+router.use('/', customerRouter);
+router.use('/', employeeRouter);
+router.use('/', saleRouter);
+router.use('/', saleDetailRouter);
+router.use('/', productWarehouseRouter);
 // router.use('/productRoute',verifyToken, productRoute)
 
 
